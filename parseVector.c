8739ff1a40eecf23e2db77f6fb0fbc52fa32b790
Shams Parvez Arka <parvez6826@gmail.com>
2022-12-24 16:07:40 +0600

    memory allocation bug fixed with <limits.h> library

--- parseVector.c
+++ parseVector.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <limits.h>
+
+#define SIZE INT_MAX // INT_MAX = +2147483647
 
 int *parseVector(unsigned int n) {
-	int *vector = malloc(sizeof(int));
+	int *vector = malloc(SIZE * sizeof(int));
 	int length = (int) floor(log10(n)); // log_base10(integer) -> integer lenght
 	for (int i = 0; i <= length; i++) {
-		vector[length-i] = n % 10; //inserting elements from backward
+		vector[length-i] = n % 10; // inserting elements from backward
 		n = n / 10;
 	}
 	return vector;
