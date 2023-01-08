c9d3cfb48c82e98840acfc895fb935212a9cebd3
Shams Parvez Arka <parvez6826@gmail.com>
2023-01-08 18:59:55 +0600

    raylib implementation to load sprite sheet :: self documentation

--- load_sprite_sheet.c
+++ load_sprite_sheet.c
@@ -7,23 +7,26 @@ int main() {
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "reperation");
 	SetTargetFPS(60);
 
-	Texture2D player = LoadTexture("assets/Run.png");
+	Texture2D texture = LoadTexture("assets/Run.png");
 
-	float frame_width = (float) player.width / 7;
-	float frame_height = (float) player.height;
-	Rectangle frame = {0.0f, 0.0f, frame_width, frame_height};
+	//Define source size and pick that area form the texture
+	float source_width = (float) texture.width / 7;
+	float source_height = (float) texture.height;
+	Rectangle source_rec = {0.0f, 0.0f, source_width, source_height};
 
-	float player_position_x = (float) SCREEN_WIDTH / 2;
-	float player_position_y = (float) SCREEN_HEIGHT / 2;
-	Vector2 player_position = {player_position_x, player_position_y};
+	//Define texture position and wrap it up in a 2D-Vector
+	float texture_position_x = (float) SCREEN_WIDTH / 2;
+	float texture_position_y = (float) SCREEN_HEIGHT / 2;
+	Vector2 texture_position = {texture_position_x, texture_position_y};
 
 	while (!WindowShouldClose()) {
 		ClearBackground(RAYWHITE);
 		BeginDrawing();
-		DrawTextureRec(player, frame, player_position, WHITE);
+			DrawTextureRec(texture, source_rec, texture_position, WHITE);
 		EndDrawing();
 	}
 
+	UnloadTexture(texture);
 	CloseWindow();
 
 	return 0;
