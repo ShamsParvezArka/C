e11774275e4886ba97717775531cc3c9647a4ad9
Shams Parvez Arka <parvez6826@gmail.com>
2023-01-14 23:48:41 +0600

    typo fixed

--- parse_vector.c
+++ parse_vector.c
@@ -5,7 +5,7 @@
 
 #define SIZE INT_MAX // INT_MAX = +2147483647
 
-int16_t *parse_vector(int32_t n) {
+int16_t *parse_vector(int64_t n) {
 	int16_t *vector = malloc(SIZE * sizeof(int16_t));
 	int16_t length = (int) floor(log10(n)); // log_base10(integer) -> integer lenght
 	for (int i = 0; i <= length; i++) {
