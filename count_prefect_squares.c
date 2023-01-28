3359af5d1dbff1dfc646aea39cfb251e9ad28615
Shams Parvez Arka <parvez6826@gmail.com>
2023-01-29 02:59:56 +0600

    improved typedef

--- count_prefect_squares.c
+++ count_prefect_squares.c
@@ -4,17 +4,19 @@
 #include <errno.h>
 #include <math.h>
 
-unsigned long int count_perfect_squares(
-		double lowerLimit, 
-		double upperLimit) {
-	// sqrt(upperlimit) and round down to the previous integer
-	// sqrt(lowerLimit) and round up to the previous integer
-	// substract them and add 1
-	return (unsigned long int) (floor(sqrt(upperLimit)) - ceil(sqrt(lowerLimit)) + 1);
-}
+unsigned long count_perfect_squares(double lower_limit, double upper_limit);
 
 int main()
 {
 	printf("%lu", count_perfect_squares(3, 9)); // count_perfect_square(3,9) -> 2
 	return 0;
+}
+
+unsigned long count_perfect_squares(
+		double lower_limit, 
+		double upper_limit) {
+	// sqrt(upperlimit) and round down to the previous integer
+	// sqrt(lowerLimit) and round up to the previous integer
+	// substract them and add 1
+	return (unsigned long) (floor(sqrt(upper_limit)) - ceil(sqrt(lower_limit)) + 1);
 }
