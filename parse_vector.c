d03a14c2fd5743a1d26f5c9efca2b734f081a1ca
Shams Parvez Arka <parvez6826@gmail.com>
2023-01-29 23:26:05 +0600

    serious bug fixed

--- parse_vector.c
+++ parse_vector.c
@@ -5,7 +5,7 @@
 
 int16_t *parse_vector(int64_t n) {
 	int16_t *vector = (int16_t*) malloc(SHRT_MAX * sizeof(int16_t));
-	int16_t length = (int) floor(log10(n)); // log_base10(integer) -> integer lenght
+	int16_t length = (int) floor(log10(n)) + 1; // log_base10(integer) -> integer lenght
 	for (int i = 0; i <= length; i++) {
 		vector[length-i] = n % 10; // inserting elements from backward
 		n = n / 10;
